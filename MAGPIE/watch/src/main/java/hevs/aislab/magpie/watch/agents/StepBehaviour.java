a85d1bd739b33fce723e61b66fb1b786ce79d12a
ChristCrett <teufteuf@windowslive.com>
2017-07-03 17:50:13 +0200

    CHrist: manage dynamic area of the progress bar, manage step (not finished)

--- MAGPIE/watch/src/main/java/hevs/aislab/magpie/watch/agents/StepBehaviour.java
+++ MAGPIE/watch/src/main/java/hevs/aislab/magpie/watch/agents/StepBehaviour.java
@@ -6,10 +6,15 @@ import ch.hevs.aislab.magpie.agent.MagpieAgent;
 import ch.hevs.aislab.magpie.behavior.Behavior;
 import ch.hevs.aislab.magpie.event.LogicTupleEvent;
 import ch.hevs.aislab.magpie.event.MagpieEvent;
+import ch.hevs.aislab.magpie.support.Rule;
 import hevs.aislab.magpie.watch.HomeActivity;
 import hevs.aislab.magpie.watch.libs.Const;
+import hevs.aislab.magpie.watch.models.Alertes;
+import hevs.aislab.magpie.watch.models.CustomRules;
 import hevs.aislab.magpie.watch.models.Measure;
+import hevs.aislab.magpie.watch.repository.AlertRepository;
 import hevs.aislab.magpie.watch.repository.MeasuresRepository;
+import hevs.aislab.magpie.watch.repository.RulesRepository;
 import hevs.aislab.magpie.watch.threads.DisplayGUI;
 
 /**
@@ -29,21 +34,44 @@ public class StepBehaviour extends Behavior {
         LogicTupleEvent lte = (LogicTupleEvent) event;
         final double steps = Double.parseDouble(lte.getArguments().get(0));
 
-        //write the measure in the database
+        //write the measure in the database, only 1 time a day
 
 
-//        Measure measure=new Measure();
-//        measure.setValue1(steps);
-//        measure.setCategory(Const.CATEGORY_STEP);
-//        measure.setTimeStamp(event.getTimestamp());
-//        MeasuresRepository.getInstance().insert(measure);
+
+
+        Measure measure=new Measure();
+        measure.setValue1(steps);
+        measure.setCategory(Const.CATEGORY_STEP);
+        measure.setTimeStamp(event.getTimestamp());
+        MeasuresRepository.getInstance().insert(measure);
 
         //SET THE VALUE ON THE GUI
         HomeActivity context=((HomeActivity)getContext());
         Runnable threadGUI=new Thread(new DisplayGUI(context, Const.CATEGORY_STEP,steps));
         context.runOnUiThread(threadGUI);
 
-        //TODO: DEFINE THE RULES FOR THE STEPS
+        //APPLY THE RULES FOR THE STEP
+
+        //Get the rules
+        CustomRules stepRules= RulesRepository.getInstance().getByCategory(Const.CATEGORY_STEP);
+
+        //get min and max value allowed, based on the rules
+        double minValue=stepRules.getVal_1_min();
+        double maxValue=stepRules.getVal_1_max();
+
+        //now we apply the rules
+
+
+
+        //no alert trigered if the steps are in the range
+        if (steps>minValue ||steps<maxValue)
+            return;
+
+        //insert the alere in the db, but we don't trigger any alert in this case.
+        Alertes alertes=new Alertes();
+        alertes.setRule(stepRules);
+        alertes.setMeasure(measure);
+        AlertRepository.getINSTANCE().insert(alertes);
 
 
     }
